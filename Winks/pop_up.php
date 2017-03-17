6d964a4323d7cbb21f1b8f553419c3a0ff4b4db3
Otuikor Samuel Emmanuel <otuikorsamuel@live.com>
2017-03-17 02:00:45 +0100

    Add files via upload

---
+++ Winks/pop_up.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<div class="row">
+        <div class="modal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Modal title</h4>
+              </div>
+              <div class="modal-body">
+                <p>One fine body…</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div>
+	</div>
+</div>
